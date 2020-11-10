78fce5c4124c233239fba80b2cce532fda8d6409
Shreyas Ikhar <shreyasikhar26@gmail.com>
2020-11-10 11:38:39 +0530

    Completed Contacts App with firebase

---
+++ src/layout/Footer.js
@@ -0,0 +1,12 @@
+//TODO: DONE: Export the Footer
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-info text-center text-white pt-3 pb-3 mt-5 sticky-bottom">
+      A simple Contact App
+    </footer>
+  );
+};
+
+export default Footer;
